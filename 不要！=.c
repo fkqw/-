956e52b26cf924a3b91d09362953ad2324349563
Jack Fe <690170846@qq.com>
2018-06-28 21:39:46 +0800

    原版

---
+++ 不要！=.c
@@ -0,0 +1,34 @@
+	if (c != '\r')
+				{
+					buf[i++] = c;
+
+				} else
+				{
+					//这里要判断是\n
+					j = recv(st, &c, 1, MSG_PEEK);
+					if ((c == '\n'))
+						j = recv(st, &c, 1, 0);
+					else
+					{
+						j = recv(st, &c, 1, 0);
+						buf[i++] = c;
+					}
+
+				}
+
+//尽量先写等于
+	if (c == '\r')
+			{
+				j = recv(st, &c, 1, MSG_PEEK);
+				if ((c == '\n'))
+					j = recv(st, &c, 1, 0);
+				else
+				{
+					//收到换行后，就结束！！！！！
+					j = recv(st, &c, 1, 0);
+					buf[i++] = c;
+				}
+			} else
+			{
+				buf[i++] = c;
+			}
