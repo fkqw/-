35350df348c0043b128be794fd7773b3a10a0c49
Jack Fe <690170846@qq.com>
2018-07-03 21:28:52 +0800

    socket客户端连接
    63行 原本

--- 不要！=.c
+++ 不要！=.c
@@ -36,3 +36,51 @@
 
 =============================================================================
 同一设备，定义结构体，相当于C++中类，不要定义过多变量
+
+
+==============================================================================
+
+		while(numbers>0)
+		{
+			numbers=get_line(fd, buf, sizeof(buf));
+			if(strcasecmp(method,"Content-Length")==0)
+			{
+				length=buf[16];
+			}
+		}
+
+		改成
+		
+		while(numbers>0)
+		{
+			numbers=get_line(fd, buf, sizeof(buf));
+			if(strcasecmp(method,"Content-Length")==0)
+			{
+				length=buf[16];
+			}
+		}
+
+==============================================		==============================================		==============================================
+		#ifdef DEBUG
+	printf("=======================\n");
+#		endif
+	   int st = socket(AF_INET, SOCK_STREAM, 0);
+	   if(st<0)
+	   {
+		   perror(" socket  failed\n");
+		   return -1;
+	   }
+	   struct sockaddr_in addr;
+	   memset(&addr,0,sizeof(addr));
+	   addr.sin_family=AF_INET;
+	   addr.sin_port=htons(clientPort);
+	   addr.sin_addr_s_addr=inet_addr(host);
+	   if(connect(st,(struct sockaddr *)&addr,sizeof(addr))==-1)
+		{
+			  perror(" connect  failed\n");
+					   return -1;
+		}
+
+	   return st;
+
+	  
